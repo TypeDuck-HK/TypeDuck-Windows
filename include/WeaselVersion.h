739dc72b482f5a18b25621575ffb65b43317e2a3
graphemecluster <graphemecluster@gmail.com>
2023-08-16 18:50:15 +0800

    Cut build 0.0.0-dev.2

--- include/WeaselVersion.h
+++ include/WeaselVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define WEASEL_VERSION_STR "0.0.0-dev.1"
-#define WEASEL_VERSION_NUM 0,0,0,1
+#define WEASEL_VERSION_STR "0.0.0-dev.2"
+#define WEASEL_VERSION_NUM 0,0,0,2
 
 #define WEASEL_CODE_NAME "TypeDuck"
 #define WEASEL_VERSION WEASEL_VERSION_STR
