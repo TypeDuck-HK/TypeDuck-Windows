acd7489a782a91d7febdde2a3af6b5ef1ee21c9d
graphemecluster <graphemecluster@gmail.com>
2025-01-04 01:36:17 +0800

    Release Build 1.1.4

--- include/WeaselVersion.h
+++ include/WeaselVersion.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-#define WEASEL_VERSION_STR "1.1.3"
-#define WEASEL_VERSION_NUM 1,1,3,0
+#define WEASEL_VERSION_STR "1.1.4"
+#define WEASEL_VERSION_NUM 1,1,4,0
 
 #define WEASEL_CODE_NAME "TypeDuck"
 #define WEASEL_VERSION WEASEL_VERSION_STR
